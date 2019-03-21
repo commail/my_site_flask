99443179dce23ef7c72031624b0d78702872c77c
chumjazz <pangzizhuo@gexi.ltd>
2019-03-21 17:50:40 +0800

    user model create

--- model/user_model.py
+++ model/user_model.py
@@ -4,7 +4,25 @@
 # @Author :garyhost
 # @File :user_model.py
 from app import db
+from manage import app
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 
 
-class Role(db.Model):
-    pass
+class User(db.Model):
+    confirmed = db.Column(db.Boolean, default=False)
+
+    def generate_confirmation_token(self, expiration=3600):
+        s = Serializer(app.config['SECRET_KEY'], expiration)
+        return s.dumps(app.config['SECRET_KEY'])
+
+    def confirm(self, token):
+        s = Serializer(app.config['SECRET_KEY'])
+        try:
+            data = s.loads(token)
+        except:
+            return False
+        if data.get('confirm') != self.id:
+            return False
+        self.confirmed = True
+        db.session.add(self)
+        return True
