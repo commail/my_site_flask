0693af530bb5af40a793434f641a1d2ef90c2721
chumjazz <pangzizhuo@gexi.ltd>
2019-03-25 18:40:56 +0800

    add gevent

--- manage.py
+++ manage.py
@@ -1,11 +1,17 @@
 from flask_script import Manager, Server
 from app import create_app
+from gevent.pywsgi import WSGIServer
 
-app = create_app('develop')
-manager = Manager(app)
-server = Server(host='0.0.0.0', port=7777)
-manager.add_command('runserver', server)
+env = 'develop'
+app = create_app(env)
 
 
 if __name__ == '__main__':
-    manager.run()
+    if env == 'develop':
+        manager = Manager(app)
+        server = Server(host='0.0.0.0', port=7777)
+        manager.add_command('runserver', server)
+        manager.run()
+    else:
+        http_server = WSGIServer(('0.0.0.0', 7777), app)
+        http_server.serve_forever()
